9cf63ee0b72614843a93219b7fd98278cc04f83c
clangcn <clangcn@gmail.com>
2017-09-21 20:55:25 +0800

    frps 1.4.13

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "内网穿透利器，谁用谁知道。", 
     "home_url": "Module_frps.asp", 
-    "md5": "b25a8cc29261eb10be678368863cce6a", 
+    "md5": "cf581e11ee355e5477f6902cb9f5d869", 
     "module": "frps", 
     "title": "frps穿透服务器", 
-    "version": "1.3.13"
+    "version": "1.4.13"
 }
